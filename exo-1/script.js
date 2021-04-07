7677252afb94c1cf356c90a031acccac9b19d7d1
NNNACHID <nicolas.nachid@le-campus-numerique.fr>
2021-04-07 21:22:47 +0200

    commit de fin de journée

--- exo-1/script.js
+++ exo-1/script.js
@@ -1,3 +1,8 @@
+/*
+Javascript est un langage de programmation
+Jquery est une bibliothèque de ce langage de programmation
+*/
+
 console.log('----------');
 console.log('Exercice 1');
 console.log('----------');
