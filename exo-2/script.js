63ba31e45684d0cbab5904d17b2f0760d8d9fa82
B043d0n <b043d0n@pop-os.localdomain>
2021-04-08 10:54:15 +0200

    Exo 2 terminer

--- exo-2/script.js
+++ exo-2/script.js
@@ -2,3 +2,38 @@ console.log('----------');
 console.log('Exercice 2');
 console.log('----------');
 
+/*
+    Il existe trois principaux types de sélecteurs en jQuery, lesquels ?
+        - les balises
+        - les ID
+        - les class
+*/
+// 1
+$('#title');
+
+// 2
+$('.nav');
+
+// 3
+$('footer');
+
+// 4
+$('.input-group input');// Ou $('.input-group'); l'énoncer manque de précision.
+
+//5
+$('.glyphicon');
+
+//6
+$('label-success');
+
+//7
+$('.intro p'); // la classe intro est inexistante
+
+//8
+$('.glyphicon-time');
+
+//9
+$('.sidenav:first');
+
+//10
+$('p:nth-child(even)');
