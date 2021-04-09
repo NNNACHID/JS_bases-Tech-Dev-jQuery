79930eb9b965e665d49a987b2f5be8419831da49
NNNACHID <nicolas.nachid@le-campus-numerique.fr>
2021-04-09 15:01:46 +0200

    commit de fin de journée

--- exo-4/script.js
+++ exo-4/script.js
@@ -1,1 +1,18 @@
 console.log("exercice 4");
+
+
+// 1
+/*
+$('.btn-danger').on("click", function(){
+    window.alert("Merci nous vous tiendrons informé des différentes offres");
+});
+*/
+// 2
+
+$(".btn-danger").click(function(){
+  var str = $("input").val();
+  alert("Merci " + str + ", nous vous tiendrons informé des différentes offres");
+});
+
+// 3 
+
