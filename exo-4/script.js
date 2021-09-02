2c6b103c97ac8658730cb8f503147c215020d145
NNACHID <nicolas.nachid@le-campus-numerique.fr>
2021-09-02 14:12:02 +0200

    2 finish, begin 3

--- exo-4/script.js
+++ exo-4/script.js
@@ -1,18 +1,22 @@
 console.log("exercice 4");
-
+console.log("RUBYYYYYY!!!");
 
 // 1
 /*
 $('.btn-danger').on("click", function(){
     window.alert("Merci nous vous tiendrons informé des différentes offres");
 });
-*/
+
 // 2
 
-$(".btn-danger").click(function(){
+$("#signUpButton").click(function(){
   var str = $("input").val();
-  alert("Merci " + str + ", nous vous tiendrons informé des différentes offres");
+  window.alert("Merci " + str + ", nous vous tiendrons informé des différentes offres");
 });
 
+*/
 // 3 
 
+$("").dblclick(function(){
+  $("").hide();
+});
