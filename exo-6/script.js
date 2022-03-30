14618d3f21e1a1596849850c50d8ef3585139d7c
NNNACHID <n.n.nachid@protonmail.com>
2022-03-30 14:01:15 +0200

    exo 6 terminer

--- exo-6/script.js
+++ exo-6/script.js
@@ -1,1 +1,32 @@
 console.log("exercice 6");
+
+
+function checkEmail(email) {
+    var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email);
+}
+
+$("#connect").click(function () {
+    console.log("le bouton is clicked");
+    var recupMail = $("#email").val();
+    var recupPass = $("#password").val();
+
+    if (recupMail.length == 0) {
+        $("#warningMail").css("display", "block");
+    } else if (!checkEmail(recupMail)) {
+        $("#warningNotAMail").css("display", "block");
+    }
+
+    if (recupPass.length < 6) {
+        if (recupPass.length == 0) {
+            $("#warningPass").css("display", "block");
+        }
+        $("#warningMiniPass").css("display", "block");
+    }
+
+    if(recupMail == "hello@me.com" && recupPass =="secret8"){
+        $("#successConnected").css("display", "block");
+    }
+    
+    console.log(recupMail + " " + recupPass);
+});
